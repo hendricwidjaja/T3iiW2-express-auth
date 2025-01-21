45975a5d1f50c713e21360188edf721404e0925b
hendricwidjaja <hcwidjaja@gmail.com>
2025-01-21 20:16:45 +1100

    feat: Initialised project, setup database connection

---
+++ src/utils/database.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+// Function to connect to the DB
+async function connectDB() {
+    let databaseUrl = process.env.DATABASE_URL || `mongodb://127.0.0.1:27017/${process.env.npm_package_name}`;
+
+    try {
+        await mongoose.connect(databaseUrl);
+        console.log("Connected to the DB!");
+    } catch (error) {
+        console.error("Error connecting to MongoDB: ", error);
+        process.exit(1);
+    }
+}
+
+module.exports = { connectDB };
